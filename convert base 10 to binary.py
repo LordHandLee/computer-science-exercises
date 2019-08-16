3787d32f8c005154912132eb9356d4d3a91220be
LordHandLee <leeethan186@gmail.com>
2019-08-16 18:33:20 -0400

    Converts a base ten number to binary.

---
+++ convert base 10 to binary.py
@@ -0,0 +1,20 @@
+while True:
+    number_to_convert = int(input("Enter any number: "))
+    binary = []
+
+    def convert_from_base_ten_to_binary(number):
+
+        newnumber =  number // 2
+        print(newnumber)
+        binary.append(number%2)
+        #print(str(binary))
+        #print(format(newnumber, '.0f'))
+        if 0 < newnumber < 2:
+            print(str(binary))
+            return
+        else:
+            convert_from_base_ten_to_binary(newnumber)
+
+
+
+    convert_from_base_ten_to_binary(number_to_convert)
